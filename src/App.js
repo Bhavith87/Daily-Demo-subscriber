e228518f6e551c0afc33dcf4205a00b85bd885da
Bhavith87 <bhavith.n@astrolitetech.com>
2025-06-30 16:38:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import DailySubscriber from './components/Daily-subscriber';
+function App() {
+  return (
+    <div className="App">
+    <DailySubscriber/>
+    </div>
+  );
+}
+
+export default App;
